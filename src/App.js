0ce67f2915c8566d01fe53ccc1c229d494d5bda8
Juan Martins <juanctmartins117@gmail.com>
2026-03-22 14:59:17 -0300

    Começando desenvolvimento do projeto

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprenda React
+          Projeto Organo
         </a>
       </header>
     </div>
