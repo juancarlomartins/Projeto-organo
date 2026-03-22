e7296610a25c0597fb878ec9b36bd62110ca3d96
Juan Martins <juanctmartins117@gmail.com>
2026-03-22 15:39:54 -0300

    Criando o componente do banner principal do site

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Olá seja bem-vindo ao meu site.</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,8 @@
+//Criando nosso primeiro componente React.
+function Banner() {
+    return (
+        <img src="/imagens/banner.png" alt="O Banner principal do Organo." />
+    )
+}
+
+export default Banner
