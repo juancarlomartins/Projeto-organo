72a5e246c8b2e3205701aeeacd3ffe8259e83dd2
Juan Martins <juanctmartins117@gmail.com>
2026-03-23 19:14:20 -0300

    Estlizando o banner do site

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,9 +1,13 @@
 //Criando nosso primeiro componente React.
+import './Banner.css'
 function Banner() {
     // JSX como o react trabalha com o visual, parece html, mas não é
     return (
-        <img src="/imagens/banner.png" alt="O Banner principal do Organo." />
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O Banner principal do Organo." />
+        </header>
     )
 }
 
 export default Banner;
+//className é usado para arquivo JavaScript.
