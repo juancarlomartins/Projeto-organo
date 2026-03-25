161df803ba2b89cb2fa1b51a17e37eeb4c2e57e8
Juan Martins <juanctmartins117@gmail.com>
2026-03-24 22:34:06 -0300

    Estrutura do formulário de cadastro

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Banner from './componentes/Banner/Banner'; //É obrigatório importar primeiro todos os componentes que se for utilizar naquela página.
-import CampoTexto from './componentes/campoTexto';
+import Formulario from './componentes/Formulário';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder="Digite o seu nome"/>
-      <CampoTexto label="Cargo" placeholder="Digite o seu cargo"/>
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+      <Formulario />
     </div>
   );
 }
