da3d0514968cd02493c8435464ef7c949f607cc5
Juan Martins <juanctmartins117@gmail.com>
2026-03-25 22:53:35 -0300

    Estilizadno o conteiner de informações do site.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Banner from './componentes/Banner/Banner'; //É obrigatório importar primeiro todos os componentes que se for utilizar naquela página.
+import Conteiner from './componentes/Conteiner';
 import Formulario from './componentes/Formulário';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario />
+      <Conteiner />
     </div>
   );
 }
--- src/componentes/Conteiner/index.js
+++ src/componentes/Conteiner/index.js
@@ -2,9 +2,16 @@ import './Conteiner.css'
 
 const Conteiner = () => {
     return(
-        <body>
-            <section>
-                <h1>Comentários sobre o site brabo organo.</h1>
+        <body className='fundo'>
+            <section className='conteiner'>
+                <h1 className='titulo'>Sobre o projeto organo.</h1>
+                <section className='conteudo'>
+                    <p className='texto'> O projeto organo foi desenvolvido
+                        durante o estudo do curso de React na Alura, onde o 
+                        projeto foi implementado do zero até o deploy. explorando
+                        os conceitos e componentes react.
+                    </p>
+                </section>
             </section>
         </body>
     )
