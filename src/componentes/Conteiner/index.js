fe0b8a73f7a63aa99417899efd14369dac277d6d
Juan Martins <juanctmartins117@gmail.com>
2026-03-25 22:31:27 -0300

    Criando um conteiner de informações sobre o site

---
+++ src/componentes/Conteiner/index.js
@@ -0,0 +1,13 @@
+import './Conteiner.css'
+
+const Conteiner = () => {
+    return(
+        <body>
+            <section>
+                <h1>Comentários sobre o site brabo organo.</h1>
+            </section>
+        </body>
+    )
+}
+
+export default Conteiner
--- src/componentes/background/background.js
+++
@@ -1,7 +0,0 @@
-function Background(){
-    return(
-        <img src="/imagens/fundo.png" alt="Papel de parede principal do site."/>
-    )
-}
-
-export default Background;
