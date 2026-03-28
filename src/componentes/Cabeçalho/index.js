827ea8ced8624c76480497866b8f9d554f367ad2
Juan Martins <juanctmartins117@gmail.com>
2026-03-28 16:21:57 -0300

    Estrutura do cabeçalho completa

--- src/componentes/Cabeçalho/index.js
+++ src/componentes/Cabeçalho/index.js
@@ -95,11 +95,70 @@ const Cabecalho = () => {
                             onChange={(e) => setTermoPesquisa(e.target.value)}
                         />
                         <button type='submit' className='pesquisa-botao'>
-                            
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path 
+                                    d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" 
+                                    stroke="currentColor" 
+                                    strokeWidth="2" 
+                                    strokeLinecap="round" 
+                                    strokeLinejoin="round"
+                                />
+                            </svg>
                         </button>
-
                     </div>
                 </form>
+
+                {/*Botão menu mobile */}
+                <button
+                    className='menu-mobile-botao'
+                    onClick={() => setMenuAberto(!menuAberto)}
+                >
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        {menuAberto ? (
+                            <path 
+                                d="M6 18L18 6M6 6L18 18" 
+                                stroke="currentColor" 
+                                strokeWidth="2" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round"
+                            />
+                        ) : (
+                            <path 
+                                d="M3 12H21M3 6H21M3 18H21" 
+                                stroke="currentColor" 
+                                strokeWidth="2" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round"
+                            />
+                        )}
+                    </svg>
+                </button>
+
+                {/*menu mobile*/}
+                {menuAberto && (
+                    <div className='menu-mobile'>
+                        <button
+                            className='botao-menu-mobile'
+                            onClick={() => handleMenuClick('Início')}
+                        >
+                            Início
+                        </button>
+
+                        <button
+                            className='botao-menu-mobile'
+                            onClick={() => handleMenuClick('Sobre')}
+                        >
+                            Sobre
+                        </button>
+
+                        <button
+                            className='botao-menu-mobile'
+                            onClick={() => handleMenuClick('Contato')}
+                        >
+                            Contato
+                        </button>
+                    </div>
+                )}
             </div>
         </header>
     )
