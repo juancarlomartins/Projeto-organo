ea295529778940e6257298da581104124707cc5c
Juan Martins <juanctmartins117@gmail.com>
2026-03-30 21:15:48 -0300

    Trecho ainda em desenvolvimento

--- src/componentes/Botao/index.js
+++ src/componentes/Botao/index.js
@@ -1,5 +1,7 @@
 import './Botao.css'
 
+
+
 const Botao = (props) => {
     return (<button className='botao'>
         {props.children}
--- src/componentes/Formulário/index.js
+++ src/componentes/Formulário/index.js
@@ -17,7 +17,7 @@ const Formulario = () => {
 
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
         <CampoTexto label="Nome" placeholder="Digite o seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite o seu cargo" />
