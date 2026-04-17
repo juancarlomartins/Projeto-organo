fcb124e1efa6a7a1e7e799ce8d3f9177c8bfd5fb
Juan Martins <juanctmartins117@gmail.com>
2026-04-16 21:53:17 -0300

    Ajustes no visual do footer

--- src/componentes/footer/footer.js
+++ src/componentes/footer/footer.js
@@ -1,78 +1,77 @@
-import React from "react";
+// Footer.jsx
+import React from 'react';
 import './footer.css';
 
 const Footer = () => {
+  // Atalhos do menu
+  const menuLinks = [
+    { name: 'Home', path: '/' },
+    { name: 'Sobre', path: '/about' },
+    { name: 'Serviços', path: '/services' },
+    { name: 'Contato', path: '/contact' }
+  ];
 
-    //criando os atalhos do menu.
-    const menuLinks = [
-        {name : 'Home', path: '/'},
-        {name: 'Sobre', path: 'about'},
-        {name: 'Serviços', path: '/services'},
-        {name: 'Contato', path: '/contact'}
-    ];
+  // Redes sociais
+  const socialLinks = [
+    { name: 'Instagram', icon: '', url: 'https://instagram.com' },
+    { name: 'Facebook', icon: '', url: 'https://facebook.com' },
+    { name: 'Twitter', icon: '', url: 'https://twitter.com' },
+    { name: 'LinkedIn', icon: '', url: 'https://linkedin.com' }
+  ];
 
-    //Redes sociais do site
-    const socialLinks = [
-        {name: 'Instagram', icon: '', url: 'https://instagram.com'},
-        {name: 'Linkedin', icon: '', url: 'https://linkedin.com'},
-        {name: 'YouTube', icon: '', url: 'https://youtube.com'},
-        {name: 'Twitter(X)', icon: '', url: 'https://x.com'}
-    ];
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        {/* Coluna Esquerda - Nome do Site */}
+        <div className="footer-col footer-col-left">
+          <h3 className="site-name">Organo</h3>
+          <p className="site-description">
+            Criando soluções inovadoras para o seu negócio.
+          </p>
+        </div>
 
-    return (
-        <footer className="footer">
-            <div className="footer-conteiner">
-                {/*Coluna da esquerda - nome do site */}
-                <div className="footer-col footer col-left">
-                    <h3 className="site-name">Organo</h3>
-                    <p className="site-description">
-                        Sistema de cadastro empresarial.
-                    </p>
-                </div>
+        {/* Coluna Centro - Redes Sociais */}
+        <div className="footer-col footer-col-center">
+          <h4 className="col-title">Redes Sociais</h4>
+          <div className="social-links">
+            {socialLinks.map((social, index) => (
+              <a
+                key={index}
+                href={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="social-link"
+                aria-label={social.name}
+              >
+                <span className="social-icon">{social.icon}</span>
+                <span className="social-name">{social.name}</span>
+              </a>
+            ))}
+          </div>
+        </div>
 
-                {/*coluna central - redes sociais */}
-                <div className="footer-col footer-col-center">
-                    <h4 className="col-title">Redes sociais</h4>
-                    <div className="social-links">
-                        {socialLinks.map((social, index) => (
-                            <a
-                                key={index}
-                                href={social.url}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="social-link"
-                                aria-label={social.name}
-                            >
-                                <span className="social-icon">{social.icon}</span>
-                                <span className="social-name">{social.name}</span>
-                            </a>
-                        ))}
-                    </div>
-                </div>
+        {/* Coluna Direita - Botões do Menu */}
+        <div className="footer-col footer-col-right">
+          <h4 className="col-title">Menu</h4>
+          <div className="menu-buttons">
+            {menuLinks.map((link, index) => (
+              <a key={index} href={link.path} className="menu-button">
+                {link.name}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
 
-                {/* Coluna da direita - botões do menu */}
-                <div className="footer-col footer-col-right">
-                    <h4 className="col-title">Menu</h4>
-                    <div className="menu-buttons">
-                        {menuLinks.map((link, index) => (
-                            <a key={index} href={link.path} className="menu-button">
-                                {link.name}
-                            </a>
-                        ))}
-                    </div>
-                </div>
-
-                {/*Divior e copyright */}
-                <div className="footer-bottom">
-                    <div className="divider">
-                        <p className="copyright">
-                            {new Date().getFullYear()} - Organo. Todos os direitos reservados.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    );
+      {/* Divisor e Copyright */}
+      <div className="footer-bottom">
+        <div className="divider"></div>
+        <p className="copyright">
+          © {new Date().getFullYear()} Organo. Todos os direitos reservados.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
