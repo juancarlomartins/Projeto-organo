ea902f057c480840d80c4e2871d10496fa0a6ebc
Juan Martins <juanctmartins117@gmail.com>
2026-04-23 22:31:21 -0300

    edição para adicionar o colaborador

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner/Banner'; //É obrigatório importar primeiro todos os componentes que se for utilizar naquela página.
 import Cabecalho from './componentes/Cabeçalho';
 import Conteiner from './componentes/Conteiner';
@@ -5,11 +6,18 @@ import Footer from './componentes/footer/footer';
 import Formulario from './componentes/Formulário';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log('Cadastro de novo colaborador: ', colaborador) // trecho ainda em edição
+  }
+
   return (
     <div className="App">
       <Cabecalho />
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
       <Conteiner />
       <Footer />
     </div>
